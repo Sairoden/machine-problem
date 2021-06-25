cf094f5cb0754ecc34dbffac8432b8a58e9391ba
Sairoden <odingandarosa@gmail.com>
2021-06-25 20:57:53 +0800

    Styling registration/edit/login

---
+++ db/migrate/20210625123338_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :length
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_120113) do
+ActiveRecord::Schema.define(version: 2021_06_25_123338) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "length"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
